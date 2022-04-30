2135340b8afb8833b982339942921cf7d4163787
Joshua Thieme <jomthieme@gmail.com>
2022-04-30 14:02:44 -0600

    Separated some functionality into their own functions.

--- src/PasswordManager.java
+++ src/PasswordManager.java
@@ -16,64 +16,102 @@ import java.util.Scanner;
 
 // TODO:
 // Separate into functions
-// Include while loop (while choice != 4 (exit)), continue to print the menu
+// Upon first start, have user create a master password
+// Upon every start after first, have user enter in master password
+// Decrypt passwords once the master password has been entered
+// Upon exiting, have user select password safe destination
+// Save and encrypt passwords to users selected destination
 public class PasswordManager {
-    public static void display(){
+    public static void welcome(){
         // Display welcome screen
         System.out.println("Welcome to the Password Manager!");
-        System.out.println("Please select what you would like to do: ");
+    }
+
+    public static void carrot(){
+        // Display carrot
+        System.out.print("\n> ");
+    }
+
+    public static String createMasterPassword(){
+        // Initialize scanner object for master password
+        Scanner master = new Scanner(System.in);
+
+        // Initialize master password variable
+        String masterPassword;
+
+        // Display to user the need for master password
+        System.out.println("Please create your master password.");
+
+        // Display carrot
+        carrot();
+
+        // Set masterPassword to receive user input
+        masterPassword = master.next();
+
+        // Return the masterPassword
+        return masterPassword;
     }
 
     public static void menu(){
+        // Instruct user to make a selection
+        System.out.println("\nPlease select what you would like to do: ");
+
         // Display available options
         System.out.println("1. Add a new password\n2. Edit an existing password\n3. Delete an existing password\n4. Exit");
 
         // Display carrot
-        System.out.print("\n> ");
+        carrot();
     }
     public static void main(String[] args){
         // Display welcome screen
-        display();
+        welcome();
 
-        // Display available options
-        menu();
+        // Get user to create their master password
+        createMasterPassword();
 
         // Get user input for options
-        Scanner obj = new Scanner(System.in);
-        int choice = obj.nextInt();
+        Scanner choose = new Scanner(System.in);
+        String choice = null;
+        while (!"4".equals(choice)) {
+            // Display available options
+            menu();
+            choice = choose.next();
 
-        // Create password array
-        String passwords[];
-        passwords = new String[0];
+            // Create password array
+            String[] passwords;
+            passwords = new String[0];
 
-        // Determine the choice
-        switch (choice){
             // If user wants to add a password to the collection,
-            case 1:
+            if ("1".equals(choice)) {
                 // Prompt them to enter a password
                 System.out.println("Enter your password: ");
 
                 // Allow them to type in their password
-                String newPassword = obj.next();
+                String newPassword = choose.next();
 
                 // Make a copy of original array, then include an additional spot
                 passwords = Arrays.copyOf(passwords, passwords.length + 1);
 
                 // Set the new password to the new spot of the array
-                passwords[passwords.length-1] = newPassword;
+                passwords[passwords.length - 1] = newPassword;
 
                 // Display their newly entered password
-                System.out.println("Your new password is: " + newPassword);
-                break;
-            case 2:
+                System.out.println("\nYour new password is: " + newPassword + "\n");
+                choice = null;
+            }
+            if ("2".equals(choice)) {
                 System.out.println("Edit which password: ");
-                break;
-            case 3:
+                choice = null;
+            }
+            if ("3".equals(choice)) {
                 System.out.println("Delete which password? ");
+                choice = null;
+            }
+            if ("4".equals(choice)) {
+                System.out.println("Goodbye!");
                 break;
-            case 4:
-                break;
-        }
+            }
 
+        }
     }
 }
